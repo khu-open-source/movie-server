6fa4944de1ecdf0433499eb105b6cb16e08839aa
L2HYUNN <donghyuni98@gmail.com>
2022-11-12 19:28:48 +0900

    chore(morgan): add morgan for watch HTTP request logs

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import helmet from "helmet";
+import morgan from "morgan";
 
 const app = express();
+const logger = morgan("dev");
 
+app.use(logger);
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
