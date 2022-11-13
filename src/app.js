cf7f452d4c154602d25d5314cc8a6c59b175fda1
L2HYUNN <donghyuni98@gmail.com>
2022-11-13 15:52:56 +0900

    chore(express-session): add express-session for session

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const app = express();
 const logger = morgan("dev");
@@ -11,5 +12,18 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session());
+
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
 
 export default app;
