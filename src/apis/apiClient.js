fd288a51bb8d5f51c3d11982614619b88c308dac
L2HYUNN <donghyuni98@gmail.com>
2022-11-13 17:42:15 +0900

    feat(axios): add axios default settings, example code

---
+++ src/apis/apiClient.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const axiosClient = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosClient;
---
+++ src/apis/movie.js
@@ -0,0 +1,7 @@
+import axiosClient from "./apiClient";
+
+export const getNowPlayingMoives = async () => {
+  return await axiosClient.get(
+    `/movie/now_playing?api_key=${process.env.TMDB_KEY}&language=en-US`
+  );
+};
