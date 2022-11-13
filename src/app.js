d1dd05b28f7d9961eb5477bb81be33c2572a1fad
L2HYUNN <donghyuni98@gmail.com>
2022-11-13 15:22:02 +0900

    feat(cookieparser): add cookieParser for handling cookies

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const logger = morgan("dev");
@@ -9,5 +10,6 @@ app.use(logger);
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 export default app;
