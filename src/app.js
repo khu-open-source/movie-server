49616463c065333ec4bff5304ab608dd907d14de
luchang <luchang@khu.ac.kr>
2022-11-21 17:43:41 +0800

    feat(src): implement login and registration logic

--- src/app.js
+++ src/app.js
@@ -5,29 +5,35 @@ import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import globalRouter from './routers/globalRouter';
 import movieRouter from './routers/movieRouter';
+import loginRouter from './routers/loginRouter';
+import registerRouter from './routers/registerRouter';
 
+const bodyParser = require('body-parser');
 const app = express();
 const logger = morgan('dev');
 
 app.use(logger);
 app.use(helmet());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(
   session({
     resave: false,
-    saveUninitialized: false,
-    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    // secret: process.env.SESSION_SECRET,
+    secret: 'keyboard cat',
     cookie: {
       httpOnly: true,
       secure: false,
     },
   }),
 );
-
 app.use('/', globalRouter);
+app.use('/login', loginRouter);
+app.use('/register', registerRouter);
 app.use('/movie', movieRouter);
 
 export default app;
---
+++ src/controllers/register.js
@@ -0,0 +1,24 @@
+function createUser(username, password) {
+  // TODO write user info to db and return result [bool]
+  console.log(username, password);
+  return true;
+}
+
+export const register = async (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  let result = createUser(username, password);
+  if (result) {
+    res.send({
+      error_code: 0,
+      msg: 'ok',
+    });
+    return;
+  } else {
+    res.send({
+      error_code: 1,
+      msg: 'Register fail',
+    });
+    return;
+  }
+};
