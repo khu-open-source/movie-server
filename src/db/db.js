bce8d0ce81149c9355d702100978b524427fb96c
SamRyong <zackinthebox@khu.ac.kr>
2022-11-25 01:49:19 +0900

    feat(src): edit password Hashing

---
+++ src/db/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+const URL = process.env.DB_URL;
+mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose를 통해 mongo connection 알려주기
+const db = mongoose.connection;
+
+const handleOpen = () => console.log('✅ Connected to DB 🚀');
+const handleError = (error) => console.log('❌ DB Error', error);
+
+db.on('error', handleError);
+db.once('open', handleOpen);
+
+export default db;
