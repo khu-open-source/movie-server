cdfc049ded6536394fe6c24891b5d9ed838e76df
SamRyong <zackinthebox@khu.ac.kr>
2022-11-27 22:06:51 +0900

    feat(src): mongoDB connect edited, add signup and login

--- src/db/db.js
+++ src/db/db.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+
 const URL = process.env.DB_URL;
 mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
 // mongoose를 통해 mongo connection 알려주기
