f7172616649620dfca60a8c8bc1efc62fe6beb61
daminoworld <ekadls757@gmail.com>
2022-11-27 17:08:44 +0900

    feat: handlePopularMovie 생성

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,7 +1,14 @@
 import { getNowPlayingMoives } from '../apis/movie';
+import { getPopularMoives } from '../apis/movie';
 
 export const handleMoive = async (req, res) => {
   const movies = await getNowPlayingMoives();
   console.log(movies.data);
   res.send('success');
+};
+
+export const handlePopularMoive = async (req, res) => {
+  const popularMovies = await getPopularMoives();
+  console.log(popularMovies.data);
+  res.send(popularMovies.data);
 };
