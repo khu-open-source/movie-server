4422b3bf53f4983d9efe520e31132ab61f3617a3
daminoworld <ekadls757@gmail.com>
2022-11-27 17:10:15 +0900

    feat: /popular 경로 handlePopularMovie

--- src/routers/movieRouter.js
+++ src/routers/movieRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { handleMoive } from '../controllers/movieController';
+import { handleMoive, handlePopularMoive } from '../controllers/movieController';
 
 const movieRouter = express.Router();
 
 movieRouter.get('/', handleMoive);
+movieRouter.get('/popular', handlePopularMoive);
 
 export default movieRouter;
