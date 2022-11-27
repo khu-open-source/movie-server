eda1b01f4e4f7a2a1bc3175b1313fe3949c96e2e
daminoworld <ekadls757@gmail.com>
2022-11-27 17:07:48 +0900

    feat: popularMovie axiosClient 생성

--- src/apis/movie.js
+++ src/apis/movie.js
@@ -3,3 +3,7 @@ import axiosClient from './apiClient';
 export const getNowPlayingMoives = async () => {
   return await axiosClient.get(`/movie/now_playing?api_key=${process.env.TMDB_KEY}&language=en-US`);
 };
+
+export const getPopularMoives = async () => {
+  return await axiosClient.get(`/movie/popular?api_key=${process.env.TMDB_KEY}&language=en-US`);
+};
