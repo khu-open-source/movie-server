823d2697edc7c74afee72146b09bf5f06422bc90
luchang <luchang@khu.ac.kr>
2022-12-01 21:27:59 +0800

    feat(src): implement login and registration logic

--- src/routers/loginRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { login } from '../controllers/login';
-
-const loginRouter = express.Router();
-
-loginRouter.post('/', login);
-
-export default loginRouter;
--- src/routers/registerRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { register } from '../controllers/register';
-
-const registerRouter = express.Router();
-
-registerRouter.post('/', register);
-
-export default registerRouter;
