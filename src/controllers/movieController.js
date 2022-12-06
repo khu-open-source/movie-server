3285218cbaa151ba631750ceaa43e926f0bb4091
L2HYUNN <donghyuni98@gmail.com>
2022-12-06 13:35:03 +0900

    fix(eslint): fix eslint rule for ci test

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -13,37 +13,36 @@ export const handlePopularMoive = async (req, res) => {
   for (const rst in results) {
     var genreIds = results[rst]['genre_ids'];
     var genreStrings = [];
-		for (const id in genreIds) {
-			const genreId = genreIds[id];
-			genreStrings.push(genres[genreId]);
-		};
-	results[rst].genre_ids = genreStrings;
+    for (const id in genreIds) {
+      const genreId = genreIds[id];
+      genreStrings.push(genres[genreId]);
+    }
+    results[rst].genre_ids = genreStrings;
 
-	results[rst]['genreList'] = results[rst]['genre_ids'];
-	delete results[rst]['genre_ids'];
-  };
+    results[rst]['genreList'] = results[rst]['genre_ids'];
+    delete results[rst]['genre_ids'];
+  }
   res.json(results);
 };
 
 const genres = {
-	 28: 'Action',
-   12: 'Adventure',
-   16: 'Animation' ,
-   35: 'Comedy',
-   80: 'Crime',
-   99: 'Documentary', 
-   18: 'Drama', 
-   10751: 'Family', 
-   14: 'Fantasy', 
-   36: 'History', 
-   27: 'Horror', 
-   10402: 'Music', 
-   9648: 'Mystery', 
-   10749: 'Romance', 
-   878: 'Science Fiction', 
-   10770: 'TV Movie', 
-   53: 'Thriller', 
-   10752: 'War', 
-   37: 'Western' 
-	};
-
+  28: 'Action',
+  12: 'Adventure',
+  16: 'Animation',
+  35: 'Comedy',
+  80: 'Crime',
+  99: 'Documentary',
+  18: 'Drama',
+  10751: 'Family',
+  14: 'Fantasy',
+  36: 'History',
+  27: 'Horror',
+  10402: 'Music',
+  9648: 'Mystery',
+  10749: 'Romance',
+  878: 'Science Fiction',
+  10770: 'TV Movie',
+  53: 'Thriller',
+  10752: 'War',
+  37: 'Western',
+};
